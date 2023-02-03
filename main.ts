28c52f1ade7b5b27887181228d03c2c86e241943
diegogalv <123975129+diegogalv@users.noreply.github.com>
2023-02-03 17:27:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,35 @@
-const VEML6070_ADDR_H = 0x39;
-const VEML6070_ADDR_L = 0x38;
-const INTEGRATION_TIME_1_T = 0x1;
-const INTEGRATION_TIME_2_T = 0x2;
-const INTEGRATION_TIME_4_T = 0x3;
-const VEML6070_ADDR_ARA = 0x0C
-const MSB = 0X73;
-const LSB = 0X71;
-const refVal = 0.4;
-let fnl = 0;
-function getUVI(uv: number): number {
-
-    let uvi = refVal * (uv * 5.625) / 1000;
-    uvi = uvi* 0.04;
-    return uvi;
+function getUVI (uv: number) {
+    uvi = refVal * (uv * 5.625) / 1000
+    uvi = uvi * 0.04
+    return uvi
 }
-function setReg(command: number) {
+function init () {
+    setReg(VEML6070_ADDR_ARA)
+    basic.pause(10)
+}
+function setReg (command: number) {
     let buf = pins.createBuffer(2);
+// basic.pause(10)
+    // basic.pause(10)
     // basic.pause(10)
     // basic.pause(10)
     buf[0] = command >> 8
     buf[1] = command & 0x03
     return pins.i2cWriteBuffer(VEML6070_ADDR_L, buf)
-    }
-
-function init(){
-    setReg(VEML6070_ADDR_ARA);
-    basic.pause(10);
 }
-
-//% color=#4c6ef5 weight=25 icon="\uf043" block="Sensor UV"
+let uvi = 0
+let refVal = 0
+let VEML6070_ADDR_ARA = 0
+let fnl = 0
+let VEML6070_ADDR_H = 57
+let VEML6070_ADDR_L = 56
+let INTEGRATION_TIME_1_T = 1
+let INTEGRATION_TIME_2_T = 2
+let INTEGRATION_TIME_4_T = 3
+VEML6070_ADDR_ARA = 12
+let MSB = 115
+let LSB = 113
+refVal = 0.4
 namespace CIPUV {
 init();
     /**
